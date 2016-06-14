0d2f84b55bba765ee5f63fbfcf1291f3e11fa9b0
Panteleyev <gleb.panteleyev@mail.ru>
2016-06-14 20:58:52 +0300

    Обновить дамп БД

--- lesson5/dump/todos.sql
+++ lesson5/dump/todos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 14, 2016 at 08:52 PM
+-- Generation Time: Jun 14, 2016 at 09:57 PM
 -- Server version: 5.5.49
 -- PHP Version: 5.3.9-ZS5.6.0
 
@@ -30,27 +30,15 @@ CREATE TABLE IF NOT EXISTS `todos` (
   `id` int(255) unsigned NOT NULL,
   `text` varchar(255) NOT NULL,
   `completed` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `todos`
 --
 
 INSERT INTO `todos` (`id`, `text`, `completed`) VALUES
-(6, 'my big task', 'true'),
-(7, 'my new task', 'true'),
-(8, 'pretty task', 'true'),
-(9, 'my little task', 'true'),
-(10, 'pretty task', 'true'),
-(11, 'pretty task', 'true'),
-(12, 'my little task', 'true'),
-(13, 'my big task', 'false'),
-(14, 'my important task', 'false'),
-(15, 'my important task', 'false'),
-(16, 'my important task', 'false'),
-(17, 'my important task', 'false'),
-(31, 'ef2', 'true'),
-(34, 'wefewf', 'false');
+(36, 'New task #1', 'false'),
+(37, 'New task #2', 'true');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +58,7 @@ ALTER TABLE `todos`
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(255) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
+  MODIFY `id` int(255) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
